4f1b5efd786dd9fe23eac449ba01c5c5efdefa6f
Kickcastle <heen.ventura@hotmail.com>
2021-03-15 11:25:58 -0400

    Se agregaron funcionalidades a los botones... se agrego play list... se agrego pictures y mejoramiento del responsive.

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,38 @@
-function pressP () {
-    alert("Haz precionado play");
+let cont = 0;
+
+function inicio() {
+    const pSound = document.getElementById('sound1');
+    pSound.play();
+}
+function pausar() {
+    const paSound = document.getElementById('sound1');
+    paSound.pause();
+}
+
+function siguiente() {
+    cont = cont + 1;
+    const sounds = document.getElementsByClassName('music');
+    const music = sounds[cont];
+    music.click();
+
+}
+
+function anterior() {
+    cont = cont - 1;
+    const sounds = document.getElementsByClassName('music');
+    const music = sounds[cont];
+    music.click();
+
+}
+
+//Maneja el codigo principal de reproduccion
+
+function Reproductor(url, imgURL) {
+    const imagen = document.getElementById('pic');
+    imagen.src = imgURL;
+
+    const cancion = document.getElementById('sound1');
+    cancion.src = url;
+    cancion.play();
+
 }
